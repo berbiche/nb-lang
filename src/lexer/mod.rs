6a48c29606f80f0991955a2a11893d5c7b93f80b
berbiche <nic.berbiche@gmail.com>
2018-02-18 02:46:28 -0500

    Implémentation Tokenizer
    - Ajout d'un module `Error` pour les erreurs du lexer, celui-ci
    verra plus d'erreurs s'ajouter à son `enum Error`
    - Refactor _src/lexer.rs_ en plusieurs fichiers
    - Plusieurs tests unitaires inutiles enlevés de _lexer_
    
    TODO: Écrire les tests unitaires du _Tokenizer_

---
+++ src/lexer/mod.rs
@@ -0,0 +1,513 @@
+use self::error::{Error, LResult};
+use token::*;
+
+use itertools::Itertools;
+
+use std::iter::Peekable;
+use std::result;
+use std::str::Chars;
+use std::vec::Vec;
+
+pub mod error;
+
+/// Le Lexer est un wrapper sur un itérateur qui lit les caractères pour former des
+/// lexèmes.
+/// Celui-ci fournit plusieurs méthodes aisant l'utilisation de l'itérateur.
+/// Le Lexer est donc lui-même un itérateur, permettant le streaming des `token::Token`
+#[derive(Debug)]
+pub struct Lexer<'a> {
+    /// Caractère courant dans la séquence de caractères
+    current_char: Option<char>,
+    /// Lexème courant dans le vecteur de token
+    current_token: Option<Token>,
+    /// Vecteur contenant les erreurs rencontrés
+    errors: Vec<Error>,
+    /// L'entrée à parse, une séquence de caractères itérable
+    input: Peekable<Chars<'a>>,
+    /// Position actuelle dans le programme
+    /// `line` est incrémenté chaque fois qu'un caractère de newline est rencontré
+    /// en prenant en considération le fait que certains systèmes d'exploitation
+    /// utilise plusieurs caractères pour représenter une nouvelle ligne
+    position: Position,
+}
+
+impl<'a> Lexer<'a> {
+
+    /// Construit un Lexer depuis une chaîne de caractères
+    pub fn new<S>(input: S) -> Self
+        where
+            S: Into<&'a str>,
+    {
+        let input = input.into();
+        Lexer {
+            current_char: None,
+            current_token: None,
+            errors: Vec::new(),
+            input: input.chars().peekable(),
+            position: Position { column: 0, line: 1 },
+        }
+    }
+
+    /// Construit le prochain `token::Token` et le renvoie
+    /// Renvoie `None` si la fin de la séquence est atteint
+    /// Validation minimale se fait ici, c'est-à-dire que les nombres ne sont pas validés
+    // TODO: Convertir la plus part de cette tâche en celle d'un macro
+    pub fn read_token(&mut self) -> LResult<Token> {
+        use token::{TokenType::*, Keyword::{self, *}, Boolean::*, Number::*};
+        self.skip_whitespace();
+
+        match self.read() {
+            None => token!(EOF, self.position),
+            Some(ch) => match ch {
+                '+' => token!(Plus, self.position),
+                '-' => token!(Minus, self.position),
+                '%' => token!(Modulo, self.position),
+                '^' => token!(Power, self.position),
+                '/' => match self.peek() {
+                    Some(&ch) if ch == '*' => { // commentaire
+                        let begin = self.position;
+                        let st = self.read_comment();
+                        token!(Comment(st), begin => self.position)
+                    },
+                    _ => token!(Division, self.position),
+                },
+                '=' => match self.peek() {
+                    Some(&ch) if ch == '=' => { // EqEq
+                        let begin = self.position;
+                        self.read();
+                        token!(EqEq, begin => self.position)
+                    },
+                    _ => token!(Eq, self.position),
+                },
+                '!' => match self.peek() {
+                    Some(&ch) if ch == '=' => { // non égal
+                        let begin = self.position;
+                        self.read();
+                        token!(NotEq, begin => self.position)
+                    },
+                    _ => token!(Not, self.position),
+                },
+                '<' => match self.peek() {
+                    Some(&ch) if ch == '=' => { // plus petit que ou égal
+                        let begin = self.position;
+                        self.read();
+                        token!(LtEq, begin => self.position)
+                    },
+                    _ => token!(Lt, self.position)
+                },
+                '>' => match self.peek() {
+                    Some(&ch) if ch == '=' => { // plus grand que ou égal
+                        let begin = self.position;
+                        self.read();
+                        token!(GtEq, begin => self.position)
+                    },
+                    _ => token!(Gt, self.position)
+                },
+                '|' => match self.peek() {
+                    Some(&ch) if ch == '|' => { // ou ou
+                        let begin = self.position;
+                        self.read();
+                        token!(OrOr, begin => self.position)
+                    },
+                    _ => token!(Or, self.position),
+                },
+                '&' => match self.peek() {
+                    Some(&ch) if ch == '&' => { // et et
+                        let begin = self.position;
+                        self.read();
+                        token!(AndAnd, begin => self.position)
+                    },
+                    _ => token!(And, self.position),
+                },
+                ',' => token!(Comma, self.position),
+                ';' => token!(Semicolon, self.position),
+                '(' => token!(Lparen, self.position),
+                ')' => token!(Rparen, self.position),
+                '{' => token!(Lbracket, self.position),
+                '}' => token!(Rbracket, self.position),
+                '[' => token!(Lbrace, self.position),
+                ']' => token!(Rbrace, self.position),
+                '_' => token!(Underscore, self.position),
+                '"' => {
+                    let begin = self.position;
+                    let st = self.read_string()?;
+                    token!(Literal(st), begin => self.position)
+                },
+                ch if ch.is_alphabetic() => { // identifiant ou keyword
+                    let begin = self.position;
+                    let ident = self.read_identifier();
+
+                    if ident == "true" {
+                        token!(True, begin => self.position)
+                    }
+                    else if ident == "false" {
+                        token!(False, begin => self.position)
+                    }
+                    else {
+                        match Keyword::lookup(ident.as_ref()) {
+                            Some(token) => token!(token, begin => self.position),
+                            None => token!(Identifier(ident), begin => self.position),
+                        }
+                    }
+                },
+                ch if is_digit(&ch) => { // lit un nombre décimal/octal/etc.
+                    let begin = self.position;
+                    match (ch, self.peek()) {
+                        ('0', Some(&peeked)) => match &peeked {
+                            'b' => { // binaire
+                                self.read();
+                                let mut st = "0b".to_string();
+                                st.push_str(&self.read_number());
+                                token!(Binary(st), begin => self.position)
+                            },
+                            'o' => { // octal
+                                self.read();
+                                let mut st = "0b".to_string();
+                                st.push_str(&self.read_number());
+                                token!(Octal(st), begin => self.position)
+                            },
+                            'x' => { // hexadécimal
+                                self.read();
+                                let mut st = "0b".to_string();
+                                st.push_str(&self.read_number());
+                                token!(Hexadecimal(st), begin => self.position)
+                            },
+                            _ => token!(Decimal(self.read_number()), begin => self.position),
+                        },
+                        _ => token!(Decimal(self.read_number()), begin => self.position),
+                    }
+                },
+                _ => token!(Illegal(ch.to_string()), self.position),
+            }
+        }
+    }
+
+    /// Getter pour la position du lexer dans la séquence
+    #[inline]
+    pub fn position(&self) -> Position {
+        self.position
+    }
+
+    /// Renvoie si le caractère actuel est celui passé en argument
+    #[inline]
+    fn current_char_is(&self, other: char) -> bool {
+        self.current_char == Some(other)
+    }
+
+    /// Renvoie une erreur de type `Error::UnexpectedSymbol`
+    #[inline]
+    fn expect_char(&mut self, exp: char, unexp: Option<char>) {
+        if unexp != Some(exp) {
+            let unexp = unexp.unwrap_or('\0').to_owned();
+            let err = Error::UnexpectedSymbol { exp, unexp, pos: self.position };
+            self.errors.push(err);
+        }
+    }
+
+    /// Permet de voir le prochain caractère sans consommer le caractère
+    /// renvoie `None` si la fin de la séquence est atteinte
+    #[inline]
+    fn peek(&mut self) -> Option<&char> {
+        self.input.peek()
+    }
+
+    /// Renvoie le prochain caractère, le consommant de l'itérateur
+    /// renvoie `None` si la fin de la séquence est atteinte
+    /// Met à jour la position du lexer dans la séquence
+    // TODO(berbiche): Incrémenter la ligne au prochain call de `read`
+    // TODO(berbiche): ...plutôt que lorsqu'une fin de ligne est rencontré
+    fn read(&mut self) -> Option<char> {
+        let previous = self.current_char;
+        let current = self.input.next();
+
+        if let Some(current) = current {
+            if is_newline(&current) {
+                if let Some(previous) = previous {
+                    // si nous n'avons pas une séquence CRLF
+                    if !(previous == '\u{000D}' && current == '\u{000A}') {
+                        self.position.line += 1;
+                        self.position.column = 0;
+                    }
+                }
+            }
+            else {
+                self.position.column += 1;
+            }
+        }
+
+        self.current_char = current;
+        self.current_char
+    }
+
+    /// Permet de lire un identifiant contenant optionnellement un '?' (question mark) à la fin
+    fn read_identifier(&mut self) -> String {
+        let mut st: String = self.current_char.unwrap().to_string();
+        {
+            let iter = self.input
+                .peeking_take_while(|ch| ch.is_alphabetic() || *ch == '_');
+            st.extend(iter);
+        }
+
+        // permet d'avoir un point d'interrogation à la fin d'un identifiant
+        if let Some(&ch) = self.peek() {
+            if ch == '?' {
+                self.read();
+                st.push(ch);
+            }
+        }
+        st
+    }
+
+    /// Lit un commentaire
+    fn read_comment(&mut self) -> String {
+        if self.current_char_is('/') { // lit un commentaire de ligne
+            self.input.peeking_take_while(is_newline).collect()
+        }
+        else { // lit un commentaire de bloc
+            let mut last_ch: char = 0 as u8 as char;
+            self.input
+                .peeking_take_while(|ch| {
+                    if last_ch != '*' && *ch != '/' {
+                        last_ch = ch.clone();
+                        true
+                    }
+                    else {
+                        false
+                    }
+                })
+                .collect()
+        }
+    }
+
+    /// Permet de lire un nombre
+    #[inline]
+    fn read_number(&mut self) -> String {
+        let mut st = self.current_char.unwrap().to_string();
+        let iter = self.input.peeking_take_while(|ch| is_hex(ch) || *ch == '_');
+        st.extend(iter);
+        st
+    }
+
+    /// Lit une chaîne de caractères jusqu'à un '"' non-échappé
+    fn read_string(&mut self) -> LResult<String> {
+        // nous voulons itérer sur la séquence jusqu'à ce que nous trouvions
+        // le caractère '"' qui n'a pas le caractère d'échappe '\\' avant
+        // et que ce caractère d'échappe n'est pas échappé
+
+        // prend le premier caractère qui est '"'
+        let mut st = self.current_char.unwrap().to_string();
+
+        // nous devons connaître le caractère précédent pour savoir si échappé
+        let mut previous_ch = '\0';
+        // pour avoir la bonne position avec les newline
+        // TODO(berbiche): M'enlever une fois que le TODO du read() sera completé
+        let mut pos: Position = self.position;
+        while let Some(current_ch) = self.read() {
+            if is_newline(&current_ch) {
+                return Err(Error::UnterminatedString(pos))
+            }
+            pos = self.position;
+
+            // les caractères de contrôle sont interdits
+            if current_ch.is_control() {
+                return Err(Error::InvalidString(st, pos))
+            }
+
+            st.push(current_ch);
+
+            // si l'échappe est échappé
+            if previous_ch == '\\' && current_ch == '\\' {
+                previous_ch = '\0';
+                continue;
+            }
+
+            if previous_ch != '\\' && current_ch == '"' {
+                return Ok(st);
+            }
+
+            previous_ch = current_ch;
+        }
+
+        Err(Error::UnexpectedEOF(self.position))
+    }
+
+    /// Saute les espaces-blancs, incluant le retour à la ligne
+    #[inline]
+    fn skip_whitespace(&mut self) {
+        self.input.by_ref().skip_while(|ch| ch.is_whitespace());
+    }
+}
+
+/// Renvoie si le caractère actuel est un chiffre décimal
+#[inline]
+fn is_digit(ch: &char) -> bool {
+    ch.is_digit(10)
+}
+
+/// Renvoie si le caractère actuel est un chiffre hexadécimal
+#[inline]
+fn is_hex(ch: &char) -> bool {
+    ch.is_digit(16)
+}
+
+/// Renvoie si le caractère actuel est un chiffre octal
+#[inline]
+fn is_octal(ch: &char) -> bool {
+    ch.is_digit(8)
+}
+
+/// Renvoie `true` si le caractère est une fin de ligne
+/// Supporte les fins de ligne de plusieurs OS
+#[inline]
+fn is_newline(ch: &char) -> bool {
+    match *ch {
+        '\u{000A}'...'\u{000D}' | '\u{0085}' | '\u{2028}' | '\u{2029}' | '\0' => true,
+        _ => false,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // TODO(Nicolas): Me documenter
+    // TODO(Nicolas): Me réécrire sous forme d'un "TT-Muncher"
+    macro_rules! test_lexer {
+        (@decl $lexer:ident, $input:expr) => (
+            let mut $lexer = Lexer::new($input);
+            $lexer.read(); // initialiser 1er caractère
+        );
+
+        (@assert $fn_name:ident, $lexer:ident, $result:expr) => (
+            assert_eq!($result, $lexer.$fn_name());
+        );
+
+        (@do $lexer:ident, $skip_amount:expr) => (
+            for _ in 0..$skip_amount {
+                $lexer.read();
+            }
+        );
+
+        // test_lexer!(fn_name, ["" => "", "" => ""])
+        (@inner $fn_name:ident,
+            $( $input:expr, $result:expr, )+
+        ) => (
+            $(
+                test_lexer!(@decl lexer, $input);
+                test_lexer!(@assert $fn_name, lexer, $result);
+            )*
+        );
+
+        // test_lexer!(fn_name, 6, ["" => "", "" => ""]
+        (@inner-num $fn_name:ident, $skip_amount:expr,
+            $( $input:expr, $result:expr, )+
+        ) => (
+            $(
+                test_lexer!(@decl lexer, $input);
+                test_lexer!(@do lexer, $skip_amount);
+                test_lexer!(@assert $fn_name, lexer, $result);
+            )*
+        );
+
+        // test_lexer!(fn_name, ["" => some "", "" => some ""])
+        ($fn_name:ident, [
+            $( $input:expr => ok $result:expr, )+
+        ]) => (
+            test_lexer!(@inner $fn_name, $($input, Ok($result.to_string()),)*);
+        );
+
+        // test_lexer!(fn_name, ["" => "", "" => ""])
+        ($fn_name:ident, [
+            $( $input:expr => $result:expr, )+
+        ]) => (
+            test_lexer!(@inner $fn_name, $($input, $result,)*);
+        );
+
+        // test_lexer!(fn_name, 1, ["" => some "", "" => some ""])
+        ($fn_name:ident, $skip_amount:expr, [
+            $( $input:expr => ok $result:expr, )+
+        ]) => (
+            test_lexer!(@inner-num $fn_name, $skip_amount, $($input, Ok($result.to_string()),)*);
+        );
+
+        // test_lexer!(fn_name, 1, ["" => "", "" => ""])
+        ($fn_name:ident, $skip_amount:expr, [
+            $( $input:expr => $result:expr, )+
+        ]) => (
+            test_lexer!(@inner-num $fn_name, $skip_amount, $($input, $result,)*);
+        );
+    }
+
+    #[test]
+    fn position() {
+        test_lexer!(position, 7, [
+            "1234567" => Position::new(1, 7),
+        ]);
+    }
+
+    #[test]
+    fn position_newline() {
+        test_lexer!(position, 4, [
+            "\r\n\r\n" => Position::new(2, 0),
+            "\n\n\r\n" => Position::new(3, 0),
+            "\n\r\n\r" => Position::new(3, 0),
+            "\r\r\r\n" => Position::new(3, 0),
+            "\n\n\n\n" => Position::new(4, 0),
+            "\r\r\r\r" => Position::new(4, 0),
+        ]);
+    }
+
+    #[test]
+    fn read_string() {
+        test_lexer!(read_string, 6, [
+            r#"voici "une longue chaîne de caractères valide"<-FIN"#
+                => ok r#""une longue chaîne de caractères valide""#,
+        ]);
+    }
+
+    #[test]
+    fn read_string_escaped() {
+        test_lexer!(read_string, [
+            r#""longue chaîne doublement \" échappé \\"<-FIN"#
+                => ok r#""longue chaîne doublement \" échappé \\""#,
+        ]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn read_string_newline() {
+        test_lexer!(read_string, [
+            "\"ouah j'ai un retour à la ligne juste ici ->\n\"<-FIN"
+                => ok "\"ouah j'ai un retour à la ligne juste ici ->\n\"",
+        ]);
+    }
+
+    #[test]
+    fn read_identifier() {
+        test_lexer!(read_identifier, [
+            "allo-ne me lit pas" => "allo",
+        ]);
+    }
+
+    #[test]
+    fn read_identifier_with_question_mark() {
+        test_lexer!(read_identifier, [
+            "test? allo" => "test?",
+        ]);
+    }
+
+    #[test]
+    #[ignore]
+    fn tokenize() {
+        use token::{
+            Token,
+            TokenType::{self, *},
+            Boolean::{self, *},
+            Number::{Self, *},
+        };
+
+        test_lexer!(read_token, [
+        ])
+    }
+}
+
--- src/token.rs
+++ src/token.rs
@@ -1,13 +1,85 @@
-use lexer::Position;
+use phf;
 
-#[derive(PartialEq, Debug, Clone)]
+use std::convert;
+use std::fmt;
+
+use self::{Keyword::*, ReservedKeyword::*};
+
+static KEYWORDS: phf::Map<&'static str, Keyword> = phf_map! {
+    "alias" => Reserved(Alias),
+    "array" => Reserved(Array),
+    "break" => Break,
+    "case" => Reserved(Case),
+    "class" => Reserved(Class),
+    "const" => Const,
+    "continue" => Continue,
+    "do" => Do,
+    "final" => Reserved(Final),
+    "else" => Else,
+    "elseif" => Elseif,
+    "export" => Reserved(Export),
+    "fun" => Fun,
+    "if" => If,
+    "import" => Reserved(Import),
+    "in" => Reserved(In),
+    "let" => Let,
+    "macro" => Reserved(Macro),
+    "of" => Reserved(Of),
+    "override" => Reserved(Override),
+    "private" => Reserved(Private),
+    "protected" => Reserved(Protected),
+    "pub" => Reserved(Pub),
+    "public" => Reserved(Public),
+    "pure" => Reserved(Pure),
+    "return" => Return,
+    "static" => Reserved(Static),
+    "struct" => Struct,
+    "switch" => Reserved(Switch),
+    "this" => Reserved(This),
+    "trait" => Reserved(Trait),
+    "unless" => Unless,
+    "use" => Reserved(Use),
+    "virtual" => Reserved(Virtual),
+    "while" => While,
+    "yield" => Reserved(Yield),
+};
+
+/// Représente un lexème dans le programme
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Token {
+    token_type: TokenType,
+    location: PositionOrSpan,
+}
+
+impl Token {
+    pub(crate) fn new(token_type: TokenType, loc: PositionOrSpan) -> Self {
+        Token {
+            token_type,
+            location: loc,
+        }
+    }
+}
+
+macro_rules! token {
+    // token!(TokenType::Test, "test", PositionOrSpan::Span(Position::new(), Position::new()))
+    ($tokentype:expr, $begin:expr => $end:expr) => {{
+        let loc = Span::new($begin, $end);
+        token!($tokentype, loc)
+    }};
+
+    // token!(TokenType::Test, "test", PositionOrSpan::Position(Position::new(1, 1)))
+    ($tokentype:expr, $loc:expr) => {{
+        let tok = Token::new($tokentype.into(), $loc.into());
+        Ok(tok)
+    }};
+}
+
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum TokenType {
-    Illegal,
-    EOF,        //
-    Identifier, // abcdef
+    EOF,
     Underscore, // _
 
-    Assign,   // =
+    Eq,       // =
     Plus,     // +
     Minus,    // -
     Division, // /
@@ -22,6 +94,8 @@ pub enum TokenType {
     LtEq,  // <=
     GtEq,  // >=
 
+    Or, // |
+    And, // &
     OrOr,   // ||
     AndAnd, // &&
 
@@ -34,75 +108,198 @@ pub enum TokenType {
     Lbrace,    // [
     Rbrace,    // ]
 
-    Comment(Comment),
-    Keyword(Keywords),
+    Illegal(String),
+    Identifier(String), // abcdef
+    Comment(String),
+    Keyword(Keyword),
     Boolean(Boolean),
+    Literal(String),
+    Number(Number),
 }
 
-#[derive(PartialEq, Debug, Clone)]
-pub enum Comment {
-    Line, // //
-    Block(BlockComment),
+impl From<Keyword> for TokenType {
+    fn from(keyword: Keyword) -> Self {
+        TokenType::Keyword(keyword)
+    }
 }
 
-#[derive(PartialEq, Debug, Clone)]
-pub enum BlockComment {
-    Start, // /*
-    End,   // */
+impl From<Boolean> for TokenType {
+    fn from(boolean: Boolean) -> Self {
+        TokenType::Boolean(boolean)
+    }
 }
 
-/// Includes reserved keywords
-#[derive(PartialEq, Debug, Clone)]
-pub enum Keywords {
-    Alias,
-    Array,
+impl From<Number> for TokenType {
+    fn from(number: Number) -> Self {
+        TokenType::Number(number)
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum Keyword {
+    Reserved(ReservedKeyword),
     Break,
-    Case,
-    Class,
     Const,
     Continue,
     Do,
     Else,
     Elseif,
-    Export,
     Fun,
     If,
+    Let,
+    Return,
+    Struct,
+    Unless,
+    While,
+}
+
+impl Keyword {
+    /// Permet de chercher un mot-clé
+    pub(crate) fn lookup(keyword: &str) -> Option<Keyword> {
+        KEYWORDS.get(keyword).cloned()
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum ReservedKeyword {
+    Alias,
+    Array,
+    Case,
+    Class,
+    Export,
+    Final,
     Import,
     In,
-    Let,
+    Macro,
     Of,
+    Override,
     Private,
     Protected,
     Pub,
     Public,
-    Return,
+    Pure,
     Static,
-    Struct,
     Switch,
-    Unless,
+    This,
+    Trait,
     Use,
-    While,
+    Virtual,
+    Yield,
 }
 
-#[derive(PartialEq, Debug, Clone)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Boolean {
-    TRUE,
-    FALSE,
+    True,
+    False,
 }
 
-#[derive(PartialEq, Debug, Clone)]
-pub struct Token<'a> {
-    token_type: TokenType,
-    literal: &'a str,
-    position: Position,
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub enum Number {
+    Binary(String),
+    Decimal(String),
+    Hexadecimal(String),
+    Octal(String),
 }
 
-impl<'a> Default for Token<'a> {
-    fn default() -> Self {
-        Token {
-            token_type: TokenType::Illegal,
-            literal: "",
-            position: Position::new(0, 0),
+/// Représente une position dans un programme
+/// Peut être employé pour attacher de l'information sur un lexème ou autre
+/// IMPORTANT: Position n'est pas relatif à l'entrée, c'est-à-dire
+/// que la position ne représente pas un byte précis dans l'entrée
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Position {
+    /// Ligne
+    pub(crate) line: usize,
+    /// Colonne
+    pub(super) column: usize,
+}
+
+impl Position {
+    pub(crate) fn new(line: usize, column: usize) -> Self {
+        Position {
+            line,
+            column,
         }
+    }
+
+    /// Combine 2 `Position`s en un `Span`
+    /// Si le `lhs` est plus petit alors il est la position de début,
+    /// sinon si les deux positions sont identiques ou `lhs` est plus grand
+    /// que `rhs` alors `Err` est renvoyé.
+    pub(crate) fn combine_to_span(lhs: Position, rhs: Position) -> Result<Span, ()> {
+        use std::cmp::Ordering::*;
+        match lhs.cmp(&rhs) {
+            Less => Ok(Span {
+                begin: lhs,
+                end: rhs,
+            }),
+            Equal | Greater => Err(()),
+        }
+    }
+
+    pub fn line(&self) -> usize { self.line }
+
+    pub fn column(&self) -> usize { self.column }
+}
+
+impl fmt::Display for Position {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.line, self.column)
+    }
+}
+
+/// Représente une gamme de caractères
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Span {
+    begin: Position,
+    end: Position,
+}
+
+impl Span {
+    /// Créé un nouveau `Span` avec les arguments passés en paramètre
+    pub(crate) fn new(begin: Position, end: Position) -> Self {
+        Span {
+            begin,
+            end,
+        }
+    }
+
+    /// Étend une gamme `Span` jusqu'à une position `Position`
+    /// Renvoie `Result` indiquant le succès de l'opération.
+    /// Renvoie `Err` si la position `rhs` est avant la fin de la gamme.
+    pub(crate) fn extend_to(&mut self, rhs: Position) -> Result<(), ()> {
+        use std::cmp::Ordering::*;
+        match self.end.cmp(&rhs) {
+            Less => {
+                self.end = rhs;
+                Ok(())
+            },
+            Equal => Ok(()),
+            Greater => Err(()),
+        }
+    }
+}
+
+impl fmt::Display for Span {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} - {}", self.begin, self.end)
+    }
+}
+
+/// Représente une gamme ou une position dans la séquence de caractères
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum PositionOrSpan {
+    Position(Position),
+    Span(Span),
+}
+
+impl convert::From<self::Span> for PositionOrSpan {
+    fn from(span: Span) -> Self {
+        PositionOrSpan::Span(span)
+    }
+}
+
+impl convert::From<self::Position> for PositionOrSpan {
+    fn from(pos: Position) -> Self {
+        PositionOrSpan::Position(pos)
     }
 }
