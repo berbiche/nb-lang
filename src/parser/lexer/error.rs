5b0d73098f30e070b1c5c26ed5b918321a899aeb
berbiche <nic.berbiche@gmail.com>
2018-02-24 16:32:09 -0500

    Ajout de l'erreur 'ExpectedToken'
    Cet erreur signale que l'on s'attendait à un symbole,
    mais que l'on a rencontré un symbole différent.

--- src/parser/lexer/error.rs
+++ src/parser/lexer/error.rs
@@ -9,23 +9,27 @@ pub type LResult<T> = result::Result<T, Error>;
 #[derive(Debug, Eq, Fail, Hash, Ord, PartialEq, PartialOrd)]
 pub enum Error {
     /// Identifiant invalide
-    #[fail(display = "Identifiant invalide: '{}' à {}", 0, 1)]
+    #[fail(display = "Identifiant invalide: '{0}' à {1}", 0, 1)]
     InvalidIdentifier(String, PositionOrSpan),
     /// Une chaîne de caractère invalide dans l'entrée
-    #[fail(display = "Chaîne de caractères invalide: '{}' à {}", 0, 1)]
+    #[fail(display = "Chaîne de caractères invalide: '{0}' à {1}", 0, 1)]
     InvalidString(String, PositionOrSpan),
     /// Début de chaîne de caractères manquant '"'
-    #[fail(display = "Début de chaîne de caractères manquant à {}", 0)]
+    #[fail(display = "Début de chaîne de caractères manquant à {0}", 0)]
     MissingStringBeginning(PositionOrSpan),
     /// End-of-file atteint avant la fin de l'opération désiré
-    #[fail(display = "End-of-File atteint avant la fin de la séquence désiré à {}", 0)]
+    #[fail(display = "End-of-File atteint avant la fin de la séquence désiré à {0}", 0)]
     UnexpectedEOF(PositionOrSpan),
     /// Le lexer s'attendait à un certain symbol, mais il en a rencontré un autre
-    #[fail(display = "Caractère inattendu: '{}' à {}", 0, 1)]
+    #[fail(display = "Caractère inattendu: '{0}' à {1}", 0, 1)]
     UnexpectedCharacter(String, PositionOrSpan),
-    #[fail(display = "Symbole inattendu: '{}' à {}", 0, 1)]
+    /// Un `Token` innatendu a été rencontré
+    #[fail(display = "Symbole inattendu: '{0}' à {1}", 0, 1)]
     UnexpectedToken(Token, PositionOrSpan),
+    /// Le `Token` attendu n'est pas celui présent
+    #[fail(display = "Symbole attendu: '{0}' plutôt que '{1}' à {2}", 0, 1, 2)]
+    ExpectedToken(Token, Token, PositionOrSpan),
     /// Chaîne de caractères non-terminée, peut-être dû à un EOF comme autre chose
-    #[fail(display = "Chaîne de caractères n'est pas terminée à {}", 0)]
+    #[fail(display = "Chaîne de caractères n'est pas terminée à {0}", 0)]
     UnterminatedString(PositionOrSpan),
 }
